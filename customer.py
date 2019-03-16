0d68b95bc847baa1e0c5369ce5971c6cecd035af
parthparikh <parthparikh1999p@gmail.com>
2019-03-16 15:52:21 +0530

    Added customer class for chatbot flow

---
+++ customer.py
@@ -0,0 +1,104 @@
+import sqlite3
+import yaml
+import random
+from hashlib import sha1
+
+class Customer:
+	def __init__(self):
+		self.name = ''
+		self.phoneNo = ''
+		self.address = ''
+		self.drop_location = ''
+		self.orders = []
+		self.bill = 0
+		self.givenDiscount = False
+		self.messages = yaml.load(open('./datasets/welcome.yml'))
+		self.last = ''
+		self.newUser = False
+		self.flag = False # becomes True when it is time to store the order
+		conn = sqlite3.connect('./customers.db')
+		self.c = conn.cursor()
+
+	def get_address_ref_code(self):
+		address_resp = self.messages['address']
+		self.address = input(random.choice(address_resp)+'\n>> ')
+		referral_resp = self.messages['referral-to-avail']
+		referral_code = input(random.choice(referral_resp)+'\n>> ')
+		ref_check = self.c.execute("SELECT uid, referral_bonus_ck FROM customers WHERE referral_code=?", (referral_code,)).fetchone()
+		if ref_check == None:
+			ref_invalid_resp = self.messages['referral-invalid']
+			print(random.choice(ref_invalid_resp))
+		elif ref_check[0] != None:
+			if ref_check[1] == False:
+				ref_repeat = self.messages['referral-repeat']
+			elif ref_check[1] == True:
+				ref_repeat = self.messages['referral']
+				print(random.choice(ref_repeat))
+				self.bill -= 100
+				self.givenDiscount = True
+				self.c.execute("UPDATE customers SET referral_bonus_ck=? WHERE uid=?", (False, ref_check[0]))
+
+	def referral_bonus_ck(self):
+		pass
+
+	def checkPhoneNo(self):
+		self.phoneNo = self.getNo()
+		count = self.c.execute("SELECT COUNT(ph_no) FROM customers WHERE ph_no=?", (self.phoneNo,)).fetchone()[0]
+		if count == 0:
+			self.newUser = True
+			self.get_address_ref_code()
+		elif count > 0:
+			prior_resp = self.messages['prior']
+			address = self.c.execute("SELECT address FROM customers WHERE ph_no=?", (self.phoneNo,)).fetchone()[0]
+			print(random.choice(prior_resp))
+			self.referral_bonus_ck()
+
+	def init_db(self):
+		if self.flag == False:
+			self.c.execute('''CREATE TABLE IF NOT EXISTS customers
+				(uid INTEGER PRIMARY_KEY,
+				name TEXT,
+				ph_no TEXT,
+				address TEXT,
+				no_of_orders INTEGER,
+				order_worth INTEGER,
+				discount_worth INTEGER,
+				priority TEXT,
+				referral_bonus_ck BIT,
+				referral_code INTEGER)''')
+
+	def user_input(self, command_line=True):
+		if command_line == True:
+			self.last = input('>> ')
+		else:
+			pass
+
+	def getNo(self):
+		no_str = 'Enter your phone no: '
+		print(no_str)
+		self.user_input()
+		hashed = sha1(self.last.encode()).hexdigest()[:10]
+		return hashed
+
+	def getName(self):
+		name_str = 'Enter your name: '
+		print(name_str)
+		self.user_input()
+		self.name = self.last
+		resp_str = 'Welcome '+self.name+'!'
+		print(resp_str)
+
+	def welcome_greeting(self):
+		welc_resp = self.messages['conversations']
+		print(random.choice(welc_resp))
+		return random.choice(welc_resp)
+
+	def execution_order(self):
+		self.init_db()
+		self.getName()
+		self.checkPhoneNo()
+		self.welcome_greeting()
+
+if __name__ == '__main__':
+	o = Customer()
+	o.execution_order()
--- maps.py
+++ maps.py
@@ -4,13 +4,13 @@ import json
 
 def calculate_dist(origin,dest):
     endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-    api_key = 'AIzaSyCG_oCYEhiir6SuQiHBoAtz6nMe70ntA9U'
+    api_key = ''
 
     request1 = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins='
     request2 = '&destinations='
-    request3 = '&key=AIzaSyCG_oCYEhiir6SuQiHBoAtz6nMe70ntA9U'
+    request3 = '&key='
 
-    request = request1+origin+request2+dest+request3
+    request = request1+origin+request2+dest+request3+api_key
 
     response = urllib.request.urlopen(request).read()
     directions = json.loads(response)
