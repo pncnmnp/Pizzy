5b9ee271045ce8e864289ebd495b3f71195586bb
iotarepeat <dhruvam15@gmail.com>
2019-03-16 14:03:29 +0530

    Created change for modifying orders

---
+++ change.py
@@ -0,0 +1,40 @@
+import ispositive
+def modify(orders:list,toRemove:str):
+    orders=orders.copy()
+    orders.remove(toRemove)
+    print("Would you like to try out these?")
+    for i,o in enumerate(orders):
+        print("%d) %s"%(i+1,o))
+    user_input = input(">>").lower()
+    if ispositive.is_positive(user_input):
+        # Modify order listing
+        print("Modify order")
+        pass
+    else:
+        # User wants to delete
+        # Greet
+        print("Okay deleting")
+        pass
+
+def change(orders:list):
+    ordinals=['first','second','third','fourth','fifth']
+    print("What do you want to change?")
+    for i,o in enumerate(orders):
+        print("%d) %s"%(i+1,o))
+    print("You can say the first one or item name")
+    user_input = input(">>").lower()
+    try:
+        i=int(user_input)
+        modify(orders,orders[i-1])
+    except ValueError:
+        for index,ordinal in enumerate( ordinals ):
+            if ordinal in user_input:
+                modify(orders,orders[index])
+        for i,o in enumerate(orders):
+            if user_input == o.lower():
+                modify(orders,o) 
+                break
+            
+
+
+change(['Pizza A','Beverage B','Item 3'])
