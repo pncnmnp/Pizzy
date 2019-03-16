e5a1446cefeb78bb877ff496a5a42e6054f6d57d
iotarepeat <dhruvam15@gmail.com>
2019-03-16 11:32:23 +0530

    Added sentiment analysis for positive/negative sentence

---
+++ ispositive.py
@@ -0,0 +1,14 @@
+from textblob import TextBlob
+import pickle
+def is_positive(sentence):
+    '''
+        Returns True if sentence is neutral/positive,False otherwise
+    '''
+    with open("datasets/words_dict.bin",'rb') as f:
+        words_dict=pickle.load(f)
+    score=0
+    for word in TextBlob(sentence).words:
+        word=word.lemmatize().lower()
+        score+=words_dict.get(word,0)
+    return score>=0 
+print(is_positive("Hello"))
